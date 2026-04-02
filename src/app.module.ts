d687b6853a994c3c6c06d248232510b7ce8180b9
mgangani <mgangani@codewinglet.com>
2026-04-02 12:35:04 +0530

    Update dependencies to Angular 21.2.7 and NestJS 11.0.1; configure TypeORM for PostgreSQL in AppModule.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,32 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller'; 
+import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, PostsModule, AuthModule],
+  imports: [
+    UsersModule,
+    PostsModule,
+    AuthModule,
+    TypeOrmModule.forRootAsync(  {
+      imports:[],
+      inject:[],
+      useFactory: () => ({
+        type: 'postgres',
+        entities:[],
+        synchronize: true,
+        host: 'localhost',
+        port: 5432,
+        username: 'postgres',
+        password: 'admin@123',
+        database: 'nestjs-practice',
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
- 
