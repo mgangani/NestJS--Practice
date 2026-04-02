e0bd4cac65c5a93a51bef75d9a523919c3dc2b54
mgangani <mgangani@codewinglet.com>
2026-04-02 11:38:16 +0530

    Update documentation and improve UsersService: enhanced coverage metrics, added descriptions, and refined SVG diagrams for better clarity. Updated UsersService methods with detailed JSDoc comments and removed unused constructor code.

--- src/users/providers/users.service.ts
+++ src/users/providers/users.service.ts
@@ -1,39 +1,53 @@
-import { forwardRef, Inject, Injectable } from "@nestjs/common";
-import { GetUsersParamDto } from "../dtos/get-users-param.dto";
-import { AuthService } from "src/auth/providers/auth.service";
+import { Injectable } from '@nestjs/common';
+import { GetUsersParamDto } from '../dtos/get-users-param.dto';
 
+
+/**
+ * UsersService is a service that provides methods to get users from the database
+ */
 @Injectable()
 export class UsersService {
+  /**
+   * Find all users
+   * @param getUsersParamDto - The parameters to get users
+   * @param page - The page number
+   * @param limit - The limit of users to return
+   * @returns An array of users
+   */
+  public findAll(
+    getUsersParamDto: GetUsersParamDto,
+    page: number,
+    limit: number,
+  ) {
+    return [
+      {
+        id: 1,
+        name: 'John Doe',
+        email: 'john.doe@example.com',
+      },
+      {
+        id: 2,
+        name: 'Alice Doe',
+        email: 'alice.doe@example.com',
+      },
+      {
+        id: 3,
+        name: 'Bob Doe',
+        email: 'bob.doe@example.com',
+      },
+    ];
+  }
 
-    constructor(@Inject(forwardRef(() => AuthService)) private readonly authService : AuthService){}
-    public findAll(getUsersParamDto : GetUsersParamDto, page : number, limit : number) {
-
-            const isAuthenticated = this.authService.isAuthenticated("token");
-            console.log(isAuthenticated);
-        return [
-            {
-                id: 1,
-                name: 'John Doe',
-                email: 'john.doe@example.com'
-            },
-            {
-                id: 2,
-                name: 'Alice Doe',
-                email: 'alice.doe@example.com'
-            },
-            {
-                id: 3,
-                name: 'Bob Doe',
-                email: 'bob.doe@example.com'
-            }
-        ]
-    }
-
-    public findOneById (id : string) {
-        return {
-            id: 1,
-            name: 'John Doe',
-            email: 'john.doe@example.com'
-        }
-    }
+  /**
+   * Find one user by id
+   * @param id - The id of the user
+   * @returns The user
+   */
+    public findOneById(id: string) {
+    return {
+      id: 1,
+      name: 'John Doe',
+      email: 'john.doe@example.com',
+    };
+  }
 }
